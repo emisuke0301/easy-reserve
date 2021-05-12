ca78bdcaf5fd037658f46321401882899515b43b
emisuke0301 <glitter777_e@yahoo.co.jp>
2021-05-12 12:29:44 +0900

    eventsコントローラーにindex,new,cteateを定義

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,26 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @events = Event.all.order('created_at DESC')
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.create(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:event_name, :date, :open_time, :start_time, :place, :price_adv, :price_door, :capacity, :genre_id, :event_text, :precautions).merge(user_id: current_user.id)
   end
 end
